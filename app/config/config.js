51757b9846edb5483739b847e2d2277144d049e1
dani0105 <danielrojas0105@gmail.com>
2022-03-21 00:18:28 -0600

    Arregla nombre de variable

--- app/config/config.js
+++ app/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
       host: process.env.DB_HOST,
       dialect: process.env.DB_DIALECT
     },
-    uri: process.env.DARABASE_URL,
+    uri: process.env.DATABASE_URL,
     logging: process.env.DB_LOGGING || false
   },
   token: {
